c40c7b3e7cee0904235a9430d0b31341a0c47e6a
daeun088 <kde2329@naver.com>
2025-02-22 00:00:40 +0900

    chore: 절대경로 및 svg 세팅

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-/**
- * Metro configuration for React Native
+/* Metro configuration for React Native
  * https://github.com/facebook/react-native
  *
  * @format
  */
 
-module.exports = {
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname);
+
+const config = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -13,5 +20,12 @@ module.exports = {
         inlineRequires: true,
       },
     }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
   },
 };
+
+module.exports = mergeConfig(defaultConfig, config);
