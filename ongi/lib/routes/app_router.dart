3d1d0e150eac2982043a8e7041e465fc5d42446a
eunseo9311 <songeunseo@soongsil.ac.kr>
2025-05-08 17:28:31 +0900

    [feat] 라우팅 설정
    보호자용 화면과 어르신용 화면의 라우팅설정

--- ongi/lib/routes/app_router.dart
+++ ongi/lib/routes/app_router.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+// 보호자용 화면
 import '../screens/helpors/home_screen.dart';
 import '../screens/helpors/alarm_screen.dart';
 
+// 어르신용 화면
+import '../screens/senior/senior_home_alarm.dart';
+
 final GoRouter router = GoRouter(
   routes: [
+    // 보호자용 홈 화면
     GoRoute(
-      path: '/',
-      name: 'home',
+      path: '/guardian-home',
+      name: 'guardian_home',
       builder: (context, state) => const HomeScreen(),
     ),
+
+    // 어르신용 홈 화면
     GoRoute(
-      path: '/alarm/:id',
-      name: 'alarm',
-      builder: (context, state) {
-        final alarmId = state.pathParameters['id'];
-        return AlarmScreen(alarmId: alarmId);
-      },
+      path: '/senior-home',
+      name: 'senior_home',
+      builder: (context, state) => const ElderHomeAlarm(),
     ),
+
   ],
 );
